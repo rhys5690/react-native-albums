3bc17a306b139b4f9ac073a84436f3c07d6cf26f
rhys5690 <rhys5690@gmail.com>
2017-07-04 11:43:50 +1000

    accessed list of albums

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -6,16 +6,20 @@ class AlbumList extends Component {
   state = { albums: [] };
 
   componentWillMount() {
-    console.log("yerere");
-    axios.get('https://rallycoding.herokuapp/api/music_albums')
+
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
       .then(response => this.setState({ albums: response.data }));
   }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <Text>{album.title}</Text>)
+  }
+
   render() {
-    console.log(this.state);
-    console.log('yer')
+    console.log(this.state)
   return(
     <View>
-      <Text>Album List</Text>
+      <Text>{this.renderAlbums()}</Text>
     </View>
     );
   }
